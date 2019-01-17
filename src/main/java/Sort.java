62c6d4e0f3235031529d1ddedf07baa4cd6705aa
Junqson <1970291214@qq.com>
2019-01-17 15:04:33 +0800

    add O(n) sort algo

--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -14,7 +14,28 @@ import java.util.Random;
  * @author Junqson
  * @date 2018/11/8 19:43
  */
+@SuppressWarnings("AlibabaCommentsMustBeJavadocFormat")
 public class Sort {
+    // --------------------O(n^2)的排序--------------------------------------------
+
+    // 选择排序 选择最小的依次放在前面有序区的最后 O(n^2)
+    public static void SelectSort(int[] arr, int n) {
+        int i, j, k, min;
+        for (i = 0; i < arr.length - 1; i++) {
+            min = i;
+            for (j = i; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                k = arr[min];
+                arr[min] = arr[i];
+                arr[i] = k;
+            }
+        }
+    }
+
 
 
     // 冒泡排序 O(n^2) 大元素向右侧的有序区进行冒泡
@@ -45,26 +66,29 @@ public class Sort {
             }
         }
     }
-
-
-
-    // 选择排序 选择最小的依次放在前面有序区的最后 O(n^2)
-    public static void SelectSort(int[] arr, int n) {
-        int i, j, k, min;
-        for (i = 0; i < arr.length - 1; i++) {
-            min = i;
-            for (j = i; j < arr.length; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
+    // 希尔排序, increment迭代方法  increament = increament / 3 + 1
+    // 插入排序优化,O(n ^ 1.3 - 2)
+    public static void shellSort(int[] arr, int start, int end) {
+        int increament = end - start + 1;
+        int temp = 0;
+        do {
+            increament = increament / 3 + 1;
+            for (int i = start + increament; i < end; i++) {
+                if (arr[i - increament] > arr[i]) {
+                    temp = arr[i];
+                    int j = i - increament;
+                    do {
+                        arr[j + increament] = arr[j];
+                        j -= increament;
+                    } while(j >= start && arr[j] > temp);
+                    arr[j + increament] = temp;
                 }
             }
-            if (min != i) {
-                k = arr[min];
-                arr[min] = arr[i];
-                arr[i] = k;
-            }
-        }
+        } while (increament > 1);
     }
+
+
+    //------------------------O(nlogn)排序算法--------------------------
 
     // 快速排序算法
     private static void qsort(int[] arr, int left, int right) {
@@ -121,25 +145,7 @@ public class Sort {
     }
 
 
-    //希尔排序, increment迭代方法  increament = increament / 3 + 1
-    public static void shellSort(int[] arr, int start, int end) {
-        int increament = end - start + 1;
-        int temp = 0;
-        do {
-            increament = increament / 3 + 1;
-            for (int i = start + increament; i < end; i++) {
-                if (arr[i - increament] > arr[i]) {
-                    temp = arr[i];
-                    int j = i - increament;
-                    do {
-                        arr[j + increament] = arr[j];
-                        j -= increament;
-                    } while(j >= start && arr[j] > temp);
-                    arr[j + increament] = temp;
-                }
-            }
-        } while (increament > 1);
-    }
+
 
 
     // 归并排序
@@ -175,46 +181,6 @@ public class Sort {
             // 将temp拷回
             System.arraycopy(temp, 0, arr, start, temp.length);
         }
-    }
-
-
-    public static void sortTime() {
-        int[] arr = new int[100000];
-        Random ra = new Random();
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = ra.nextInt(100001);
-        }
-
-        int[] arr2 = Arrays.copyOf(arr, arr.length);
-        int[] arr3 = Arrays.copyOf(arr, arr.length);
-        int[] arr4 = Arrays.copyOf(arr, arr.length);
-        int[] arr5 = Arrays.copyOf(arr, arr.length);
-
-
-        // 冒泡排序比较插入排序，数据规模比较大时，插入排序优于冒泡排序，不用做频繁的交换操作
-
-
-        long time = System.currentTimeMillis();
-        BubbleSort(arr, arr.length);
-        long time2 = System.currentTimeMillis();
-        System.out.println("bsort : " + String.valueOf(time2 - time) + "ms");
-
-        InsertSort(arr2, arr2.length);
-        long time3 = System.currentTimeMillis();
-        System.out.println("isort : " + String.valueOf(time3 - time2) + "ms");
-
-        quicksort(arr3);
-        long time4 = System.currentTimeMillis();
-        System.out.println("qsort : " + String.valueOf(time4 - time3) + "ms");
-
-        MergeSort(arr4, 0, arr.length - 1);
-        long time5 = System.currentTimeMillis();
-        System.out.println("msort : " + String.valueOf(time5 - time4) + "ms");
-
-        heapSort(arr5, arr.length);
-        long time6 = System.currentTimeMillis();
-        System.out.println("hsort : " + String.valueOf(time6 - time5) + "ms");
     }
 
 
@@ -274,6 +240,111 @@ public class Sort {
 
 
 
+    // ----------------------------o(n)排序算法---------------------------------------
+    // 下面三种：桶排序、计数排序、基数排序都是O(n)的线性排序，只适用部分特殊场景
+
+    // 桶排序
+    // 一种重要的外部排序，将元素划分到多个桶内，然后在桶内进行排序，最后直接连接起来，均匀情况下，O(n)，
+    // 分布不均匀，会退化到O(nlogn)，这时候可以对集中的桶再次进行划分
+    // 不适用负数和小数情况，可以将负数转换为正数，保持相对大小不变[-1000,1000] --(+1000)--> [0, 2000]
+    // 对于小数，可以乘10的倍数转化为整数,[-1.5 ,7.5] ------(X10)-----> [-15, 75]
+
+    public static void bucketSort() {
+        // 代码略
+    }
+
+    // 计数排序，一种桶排序的特例：桶内只有一个
+    public static void countSort() {
+        int[] arr = new int[10];
+        Random ra = new Random();
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = ra.nextInt(10);
+        }
+        int[] c = new int[10];
+        for (int i = 0; i < arr.length; ++i) {
+            ++c[arr[i]];
+        }
+        for (int i = 1; i < c.length; ++i) {
+            c[i] += c[i - 1];
+        }
+
+        int[] r = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            int index = c[arr[i]] - 1;
+            r[index] = arr[i];
+            --c[arr[i]];
+        }
+        System.out.println("ok");
+    }
+
+    // 基数排序
+    // 要求待排序数据可以划分为独立的位，位之间有递进关系，先比较高位，如果高位不同，后面的低位可以不比较
+    // 比如手机号码和字典序排序。
+    // 要求数据等长，否则可以在前面补0。
+    // 从低位开始，依次划分，可以借助桶排序或者计数排序
+    public static void radixSort() {
+        // 代码略
+    }
+
+
+
+
+    // 比较排序算法时间
+
+    public static void sortTime() {
+        int[] arr = new int[400000];
+        Random ra = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ra.nextInt(100001);
+        }
+
+        int[] arr0 = Arrays.copyOf(arr, arr.length);
+        int[] arr2 = Arrays.copyOf(arr, arr.length);
+        int[] arr3 = Arrays.copyOf(arr, arr.length);
+        int[] arr4 = Arrays.copyOf(arr, arr.length);
+        int[] arr5 = Arrays.copyOf(arr, arr.length);
+        int[] arr6 = Arrays.copyOf(arr, arr.length);
+
+
+
+        long time0 = System.currentTimeMillis();
+        SelectSort(arr0, arr.length);
+        long time = System.currentTimeMillis();
+        System.out.println("选择排序 : " + String.valueOf(time - time0) + "ms");
+
+        // 冒泡排序比较插入排序，数据规模比较大时，插入排序优于冒泡排序，不用做频繁的交换操作
+
+        BubbleSort(arr, arr.length);
+        long time2 = System.currentTimeMillis();
+        System.out.println("冒泡排序 : " + String.valueOf(time2 - time) + "ms");
+
+        InsertSort(arr2, arr2.length);
+        long time3 = System.currentTimeMillis();
+        System.out.println("插入排序 : " + String.valueOf(time3 - time2) + "ms");
+
+        // 插入排序优化：希尔排序O(n ^ 1.3 - 2)
+        shellSort(arr6, 0, arr.length - 1);
+        long time3_4 = System.currentTimeMillis();
+        System.out.println("希尔排序 : " + String.valueOf(time3_4 - time3) + "ms");
+
+
+        quicksort(arr3);
+        long time4 = System.currentTimeMillis();
+        System.out.println("快排 : " + String.valueOf(time4 - time3_4) + "ms");
+
+        MergeSort(arr4, 0, arr.length - 1);
+        long time5 = System.currentTimeMillis();
+        System.out.println("归并排序 : " + String.valueOf(time5 - time4) + "ms");
+
+        heapSort(arr5, arr.length);
+        long time6 = System.currentTimeMillis();
+        System.out.println("堆排序 : " + String.valueOf(time6 - time5) + "ms");
+    }
+
+
+
+
 
 
 
@@ -290,8 +361,9 @@ public class Sort {
 //        heapSort(arr, arr.length);
 //        quicksort(arr);
 
-        sortTime();
+//        sortTime();
 
+        countSort();
 
         System.out.println();
     }
